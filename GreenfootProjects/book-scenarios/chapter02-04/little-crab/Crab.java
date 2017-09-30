0cfcbc4727694e1e0d1d70dc8c415387772a398b
moises-limon <32418876+moises-limon@users.noreply.github.com>
2017-09-30 12:17:25 -0700

    AvoiderGameUpdate

---
+++ GreenfootProjects/book-scenarios/chapter02-04/little-crab/Crab.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+
+/**
+ * This class defines a crab. Crabs live on the beach.
+ */
+public class Crab extends Animal
+{
+    public void act()
+    {
+    }
+}
+
+
